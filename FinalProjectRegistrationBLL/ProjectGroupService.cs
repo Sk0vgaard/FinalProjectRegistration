afad5b4a61e455dccf60036b7243f0561bacf70d
Adam Hansen <adamlars90@gmail.com>
2017-09-26 10:59:11 +0200

    Started implementing ProjectGroup tests

---
+++ FinalProjectRegistrationBLL/ProjectGroupService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using FinalProjectRegistrationBE;
+using FinalProjectRegistrationBLL.Interfaces;
+
+[assembly: InternalsVisibleTo("FinalProjectRegistrationBLLShould")]
+namespace FinalProjectRegistrationBLL
+{
+    internal class ProjectGroupService : IProjectGroupService
+    {
+        public ProjectGroupBO Create(ProjectGroupBO entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProjectGroupBO Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<ProjectGroupBO> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FinalProjectRegistrationBLLShould/ProjectGroupServiceShould.cs
@@ -0,0 +1,39 @@
+﻿using FinalProjectRegistrationBLL;
+using FinalProjectRegistrationBLL.Interfaces;
+using FinalProjectRegistrationDAL;
+using Xunit;
+
+namespace FinalProjectRegistrationBLLShould
+{
+    public class ProjectGroupServiceShould : IBLLTest
+    {
+        private readonly IDALFacade _facade = new DALFacade();
+        private readonly IProjectGroupService _service;
+
+        public ProjectGroupServiceShould()
+        {
+            _service = new ProjectGroupService();
+        }
+
+        [Fact]
+        public void GetOne()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void CreateOne()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void NotCreateOne()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
