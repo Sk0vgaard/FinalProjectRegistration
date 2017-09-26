6d2c37f9d3028fa4219c2f3decfa8722584c85da
Adam Hansen <adamlars90@gmail.com>
2017-09-26 09:41:35 +0200

    Added student

--- FinalProjectRegistrationBE/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FinalProjectRegistrationBE
-{
-    public class Class1
-    {
-    }
-}
---
+++ FinalProjectRegistrationBE/StudentBO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinalProjectRegistrationBE
+{
+    public class StudentBO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
