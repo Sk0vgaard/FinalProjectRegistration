ec5862fa01cdb2ee7d2b039273e3e52342a18345
LindvedKrvang <rklindved@gmail.com>
2017-09-26 09:39:17 +0200

    Added company

---
+++ FinalProjectRegistrationBE/CompanyBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjectRegistrationBE
+{
+    public class CompanyBO
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Contact { get; set; }
+    }
+}
