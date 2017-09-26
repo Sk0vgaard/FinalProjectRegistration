0aa89a895c7e458dc558fe0ce4f0656a0ab72dd9
Skovgaard <Sk0vgaard@live.dk>
2017-09-26 10:58:08 +0200

    Added test + ProposalService class.

---
+++ FinalProjectRegistrationBLL/ProposalService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FinalProjectRegistrationBE;
+using FinalProjectRegistrationBLL.Interfaces;
+
+namespace FinalProjectRegistrationBLL
+{
+    class ProposalService : IProposalService
+    {
+        public ProposalBO Create(ProposalBO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProposalBO Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProposalBO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalProjectRegistrationBLLShould/ProposalServiceShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjectRegistrationBLLShould
+{
+    class ProposalServiceShould : IBLLTest
+    {
+        public void GetOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotCreateOne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
