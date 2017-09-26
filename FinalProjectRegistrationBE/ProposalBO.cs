99113d0fb43dd93091f1b2dc2beb31eb357255b1
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-09-26 09:39:35 +0200

    Made Proposal

---
+++ FinalProjectRegistrationBE/ProposalBO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjectRegistrationBE
+{
+    class ProposalBO
+    {
+        public string Title { get; set; }
+        public string Objectives { get; set; }
+    }
+}
