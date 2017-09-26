7de73cd23d2a8cb5865c2a40e1f8127ec3d100be
Skovgaard <Sk0vgaard@live.dk>
2017-09-26 09:48:02 +0200

    Assigment, ProjectGroup, SupervisorRequest classes.

---
+++ FinalProjectRegistrationBE/ProjectGroupBO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjectRegistrationBE
+{
+    class ProjectGroupBO
+    {
+        public string name { get; set; }
+    }
+}
